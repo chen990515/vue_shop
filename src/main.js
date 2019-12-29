93d02bc08cf958fa38dd90bdae493471f13365e4
axuan <chen@qq.com>
2019-12-29 09:04:38 +0800

    完整代码

--- src/main.js
+++ src/main.js
@@ -10,6 +10,9 @@ import Mbx from './components/Mbx.vue'
 import TreeTable from 'vue-table-with-tree-grid'
 // 导入富文本编辑器
 import VueQuillEditor from 'vue-quill-editor'
+// 导入Nprogress 包对应的JS和css
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 // require styles 导入富文本编辑器对应的样式
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
@@ -17,9 +20,17 @@ import 'quill/dist/quill.bubble.css'
 import axios from 'axios'
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 在 request 拦截器中, 展示进度条 NProgress.start()
 axios.interceptors.request.use(config => {
-  console.log(config)
+  // console.log(config)
+  NProgress.start()
   config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须return config
+  return config
+})
+// 在 response 拦截器中, 隐藏进度条 NProgress.done()
+axios.interceptors.response.use(config => {
+  NProgress.done()
   // 在最后必须return config
   return config
 })
@@ -32,7 +43,7 @@ Vue.component('tree-table', TreeTable)
 // 将富文本编辑器, 注册为全局可用的组件
 Vue.use(VueQuillEditor)
 // 注册全局过滤器
-Vue.filter('dateFormat', function(originVal) {
+Vue.filter('dateFormat', function (originVal) {
   const dt = new Date(originVal)
   const y = dt.getFullYear()
   const m = (dt.getMonth() + 1 + '').padStart(2, '0')
